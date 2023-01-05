de550918e2eaf60e2ce27a165d553156266b0a61
Martin <martin.iribass@gmail.com>
2023-01-05 11:11:02 -0300

    integrate react navigation

--- memopad/src/app.tsx
+++ memopad/src/app.tsx
@@ -6,7 +6,6 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
