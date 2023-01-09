01eb8298ffbff96e86b4fa5bed288a8ccd61290c
Martin <martin.iribass@gmail.com>
2023-01-08 23:09:05 -0300

    list items

--- memopad/src/components/sidebar.tsx
+++ memopad/src/components/sidebar.tsx
@@ -1,17 +1,65 @@
-import React from "react";
 import { Box, Text } from '@/atoms'
-import { DrawerContentComponentProps } from '@react-navigation/drawer'
-import { SafeAreaView } from "react-native";
+import activeThemeId from '@/states/theme'
+import { Theme, ThemeMeta, ThemeNames, themes } from '@/themes'
+import { DrawerNavigationHelpers } from '@react-navigation/drawer/lib/typescript/src/types'
+import { useNavigation } from '@react-navigation/native'
+import { createBox } from '@shopify/restyle'
+import { useAtom } from 'jotai'
+import React, { useCallback } from 'react'
+import { FlatList, FlatListProps, SafeAreaView } from 'react-native'
+//import InkdropLogo from './inkdrop-logo'
+import ThemeListItem from './theme-list-item'
 
-const Sidebar: React.FC<DrawerContentComponentProps> = () => {
-    return (
-        <Box flex={1} bg="$sidebarBackground">
-            <SafeAreaView>
-                <Text variant="sidebar" m='lg'>
-                    primero
-                </Text>
-            </SafeAreaView>
+type Props = {}
+const StyledFlatList = createBox<Theme, FlatListProps<ThemeMeta>>(FlatList)
+
+const Sidebar: React.FC<Props> = () => {
+  const navigation = useNavigation<DrawerNavigationHelpers>()
+  const [, setActiveTheme] = useAtom(activeThemeId)
+
+  const handleThemeItemPress = useCallback(
+    (selectedThemeId: ThemeNames) => {
+      setActiveTheme(selectedThemeId)
+      // navigation.closeDrawer()
+    },
+    [navigation]
+  )
+
+  const renderThemeItem = useCallback(
+    ({ item }: { item: ThemeMeta }) => {
+      return <ThemeListItem theme={item} onPress={handleThemeItemPress} />
+    },
+    [handleThemeItemPress]
+  )
+
+  return (
+    <Box flex={1} bg="$sidebarBackground">
+      <SafeAreaView>
+        <Box
+          alignItems="flex-start"
+          pl="md"
+          pb="sm"
+          mt="xs"
+          borderBottomColor="$sidebarSeparator"
+          borderBottomWidth={1}
+        >
+          {/* <InkdropLogo width={128} height={36} color="$sidebarForeground" /> */}
         </Box>
-    )
+      </SafeAreaView>
+      <StyledFlatList
+        ListHeaderComponent={() => (
+          <Box p="lg" alignItems="flex-start">
+            <Text color="$sidebarForeground" fontWeight="bold">
+              UI Themes
+            </Text>
+          </Box>
+        )}
+        data={themes}
+        keyExtractor={(t: ThemeMeta) => t.id}
+        renderItem={renderThemeItem}
+      />
+    </Box>
+  )
 }
-export default Sidebar;
+
+export default Sidebar
