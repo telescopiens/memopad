f7d9f2251377cd206e9e4fe2f765e41c1497430e
Martin <martin.iribass@gmail.com>
2023-01-10 17:56:48 -0300

    imp theme in sidebar

--- memopad/src/components/sidebar.tsx
+++ memopad/src/components/sidebar.tsx
@@ -1,10 +1,16 @@
+import { Box, Text } from '@/atoms'
 import { DrawerContentComponentProps } from '@react-navigation/drawer'
 import React from 'react'
 import { View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Sidebar: React.FC<DrawerContentComponentProps> = () => {
   return (
-    <View></View>
+    <Box flex={1} bg={'$sidebarBackground'}>
+      <SafeAreaView>
+        <Text variant={'sidebar'} m={'lg'}>sidebar</Text>
+      </SafeAreaView>
+    </Box>
   )
 }
 
