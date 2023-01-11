6a5d6f36ca88230af36a2c376d8b64f29c4870a9
Martin <martin.iribass@gmail.com>
2023-01-11 11:35:10 -0300

    note list item

---
+++ memopad/src/components/note-list-item.tsx
@@ -0,0 +1,17 @@
+import { Box, Text } from '@/atoms';
+import { Note } from '@/models';
+import React from 'react';
+
+export interface ListItemProps extends Note {}
+
+const NoteListItem: React.FC<ListItemProps> = props => {
+    return (
+        <Box bg="$background">
+            <Box bg="$background" px='lg' py='sm'>
+                <Text>{props.body}</Text>
+            </Box>
+        </Box>
+    )
+}
+
+export default NoteListItem
