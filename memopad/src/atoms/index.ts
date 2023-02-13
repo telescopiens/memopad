ed1ec18b2dc0ba910cb4adf256c9c777d3264616
Martin <martin.iribass@gmail.com>
2023-02-13 16:37:58 -0300

    atom comp bar

--- memopad/src/atoms/index.ts
+++ memopad/src/atoms/index.ts
@@ -1,5 +1,7 @@
 import Box from './box';
 import Text from './text';
 import Container from './container';
+import AnimatedBox from './animated-box';
+import Bar from './bar'
 
-export { Box, Text, Container }
+export { Box, Text, Container, AnimatedBox, Bar }
