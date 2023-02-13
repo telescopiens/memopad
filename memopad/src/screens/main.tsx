953d6a7c42ceaed3454d073e58d3e26b38a74b4c
Martin <martin.iribass@gmail.com>
2023-02-13 17:02:35 -0300

    header bar

--- memopad/src/screens/main.tsx
+++ memopad/src/screens/main.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { StyleSheet } from 'react-native'
 import { Container, Text } from '@/atoms'
 import NoteList from '@/components/note-list';
+import HeaderBar from '@/components/header-bar';
 
 export default function MainScreen() {
     return (
         <Container justifyContent="center" alignItems="center">
             <NoteList />
+            <HeaderBar />
         </Container>
     )
 }
