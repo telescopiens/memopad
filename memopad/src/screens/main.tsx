e5362000155a3793dcf123de312cf7edeb54ff51
Martin <martin.iribass@gmail.com>
2023-02-17 12:40:59 -0300

    header bar

--- memopad/src/screens/main.tsx
+++ memopad/src/screens/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native'
-import { Container, Text } from '@/atoms'
+import { Container, Box, TouchableOpacity, Text } from '@/atoms'
 import NoteList from '@/components/note-list';
 import HeaderBar from '@/components/header-bar';
 import FeatherIcon from '@/components/icon';
@@ -10,8 +10,15 @@ export default function MainScreen() {
         <Container justifyContent="center" alignItems="center">
             <NoteList />
             <HeaderBar>
-                <FeatherIcon name='menu' size={22}/>
-                <FeatherIcon name='list' size={22}/>
+                <TouchableOpacity m={'xs'} p={'xs'} rippleBorderless>
+                    <FeatherIcon name='menu' size={22}/>
+                </TouchableOpacity>
+                <Box flex={1} alignItems={'center'}>
+                    <Text fontWeight={'bold'}>Notas</Text>
+                </Box>
+                <TouchableOpacity m={'xs'} p={'xs'} rippleBorderless>
+                    <FeatherIcon name='list' size={22}/>
+                </TouchableOpacity>
             </HeaderBar> 
         </Container>
     )
