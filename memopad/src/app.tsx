68ec0cc7be1c600266c68547e9b637b28342269d
Martin <martin.iribass@gmail.com>
2023-03-17 10:30:01 -0300

    status bar

--- memopad/src/app.tsx
+++ memopad/src/app.tsx
@@ -3,11 +3,13 @@ import { NavigationContainer } from '@react-navigation/native'
 import Navigations from './navs';
 import light from '@/themes/light';
 import { ThemeProvider } from '@shopify/restyle';
+import StatusBar from './components/status-bar';
 
 const App = () => {
     return (
         <NavigationContainer>
             <ThemeProvider theme={light}>
+                <StatusBar />
                 <Navigations />
             </ThemeProvider>
         </NavigationContainer>
